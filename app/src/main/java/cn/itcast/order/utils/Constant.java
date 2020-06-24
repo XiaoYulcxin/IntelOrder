4ae47144392b0fa7c41a33ff2efb01f8ec2affd6
unknown <1538947053@qq.com>
2020-06-24 09:54:31 +0800

    modify

---
+++ app/src/main/java/cn/itcast/order/utils/Constant.java
@@ -0,0 +1,6 @@
+package cn.itcast.order.utils;
+
+public class Constant {
+    public static final String WEB_SITE = "http://192.168.0.4:8080/order"; //内网接口
+    public static final String REQUEST_SHOP_URL = "/shop_list_data.json"; //店铺列表接口
+}
